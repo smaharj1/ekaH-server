7d513ab725cd1db4b929fde26d22c6f29ce3c1fe
smaharj1 <smaharj1@ramapo.edu>
2017-04-11 01:31:08 -0400

    Added functionalities to POST and PUT assignment

--- ekaH-server/Controllers/AssignmentController.cs
+++ ekaH-server/Controllers/AssignmentController.cs
@@ -1,6 +1,8 @@
 ﻿using ekaH_server.App_DBHandler;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,8 +14,8 @@ namespace ekaH_server.Controllers
     {
         private ekahEntities11 db = new ekahEntities11();
 
-        // GET: ekah/assignments/{courses}/{id}
-        // Gets all the assignments of a certain course
+        /// GET: ekah/assignments/{courses}/{id}
+        /// Gets all the assignments of a certain course
         [HttpGet]
         [ActionName("courses")]
         public IHttpActionResult GetAllAssignmentsByCourse(string id)
@@ -26,6 +28,79 @@ namespace ekaH_server.Controllers
             }
 
             return Ok(assignments);
+        }
+
+        /// <summary>
+        /// POST: Makes a post call to save the assignment to the database.
+        /// </summary>
+        /// <param name="assgn"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ActionName("courses")]
+        public IHttpActionResult PostAssignmentByCourse([FromBody] assignment assgn)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (assignmentExists(assgn.courseID, assgn.projectNum))
+            {
+                return StatusCode(HttpStatusCode.Conflict);
+            }
+
+            db.assignments.Add(assgn);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbUpdateException)
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Changes the existing courses values according to the id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="assgn"></param>
+        /// <returns></returns>
+        [HttpPut]
+        [ActionName("courses")]
+        public IHttpActionResult PutAssignmentByCourse(string id, [FromBody] assignment assgn)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Entry(assgn).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch(DbUpdateException)
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Checks if the assignment already exists in the database.
+        /// </summary>
+        /// <param name="courseID">Holds the course id.</param>
+        /// <param name="projectNum">Holds the project number.</param>
+        /// <returns></returns>
+        private bool assignmentExists(string courseID, int projectNum)
+        {
+            return db.assignments.Count(assgn => assgn.courseID == courseID && assgn.projectNum == projectNum) > 0;
         }
     }
 }
